97aa2f1c0bb4f11fea7289d277510448f0e33acb
Mohamad Moughnie <mhdmoughnie@gmail.com>
2023-02-07 22:31:59 +0200

    1st

---
+++ src/components/projects/mock.js
@@ -0,0 +1,30 @@
+
+export const data = [
+  {
+      "id": 1,
+      "title": "Enterprise Resources Planning",
+      "stack": "React/Laravel and React Native",
+      "url": "https://www.youtube.com/watch?v=ZML7ygoS9q0",
+      "number": "01",
+      "image1": require("../../assets/project 1.png"),
+      "image2": require("../../assets/s1 .png")
+  },
+  {
+      "id": 2,
+      "title": "TODO Application",
+      "stack": "REACT JS and ASP.NET Core",
+      "url": "https://www.youtube.com/watch?v=SRdJP1TtMG4",
+      "number": "02",
+      "image1": require("../../assets/project 2.png"),
+      "image2": require("../../assets/react-logo.png")
+  },
+  {
+      "id": 3,
+      "title": "Bader, simple people management website",
+      "stack": "Next.js + Symphony (PHP)",
+      "url": "https://www.youtube.com/watch?v=beUw6_usbtE",
+      "number": "03",
+      "image1": require("../../assets/project 4.png"),
+      "image2": require("../../assets/s3.png")
+  }
+]
---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import './skills.css'
+import logo from '../../assets/logo-1.png';
+import sql from '../../assets/SQL.png'
+import js from '../../assets/javascript.png'
+import node from '../../assets/nodeJS.png'
+import reactpic from '../../assets/react.png'
+import python from '../../assets/python.png'
+import { useEffect, useState } from 'react';
+
+const Skills = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+     const fetchData = async () => {
+        try {
+           const response = await fetch('http://localhost:5000/skills');
+           const data = await response.json();
+           setPosts(data);
+        } catch (err) {
+           console.log(err.message);
+        }
+     };
+
+     fetchData();
+  }, []);
+  return (
+    <div>
+      
+    
+    <br />
+    <br />
+    <br />
+    <div className='skills-title'>
+      <img src={logo} />
+    <h1>Skills: </h1>
+    </div>
+    <div className='skills-container' id='skills'>
+    {posts.map(item => (
+    <div className='skills-items'>
+     <span className='skills-item-1'> 
+        <img src={`http://localhost:5000/${item.image1}`}/>
+        <h3>  {item.description}</h3>
+     </span>
+     
+    </div>))}
+
+    
+
+    </div>
+    <div className='skills-quote'>
+    {posts.map(item => (
+      <p>{item.quote}</p>
+    ))}
+
+    </div>
+    </div>
+  )
+}
+
+export default Skills
