a14ed3fc6004def77c6fd0ba74bb00f54c8581ed
Mohannad <mohannadalrifai7@gmail.com>
2023-02-12 01:28:00 +0200

    added-routes

--- src/components/certificates/Certificates.jsx
+++ src/components/certificates/Certificates.jsx
@@ -1,56 +1,82 @@
 import React, { Fragment } from "react";
 import "./certificates.css";
-import certificate1 from "../../assets/certificate1.jpg";
-import certificate2 from "../../assets/certificate2.jpg";
-import certificate3 from "../../assets/certificate3.jpg";
-import certificate4 from "../../assets/certificate4.jpg";
-import Carousel from 'better-react-carousel';
-
-
-import logo from "../../assets/logo.png"
-
-import { useEffect, useState } from "react";
-
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 550, itemsToShow: 2 },
- 
-];
+import certificate1 from "../certificates/images/certificate1.jpg";
+import certificate2 from "../certificates/images/certificate2.jpg";
+import certificate3 from "../certificates/images/certificate3.jpg";
+import certificate4 from "../certificates/images/certificate4.jpg";
+import logo from "../certificates/images/logo.png"
 
 const ProductCard = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-     const fetchData = async () => {
-        try {
-           const response = await fetch('http://localhost:5000/certificates');
-           const data = await response.json();
-           setPosts(data);
-        } catch (err) {
-           console.log(err.message);
-        }
-     };
-
-     fetchData();
-  }, []);
-  console.log(posts);
- 
-
-  
   return (
-
-
-
     <div id="certificates">
-    
-  {/* <img className="logo" src={logo} alt="logo" />
-  <p> 
-  Certificates:
-</p>
-{posts.map(item => (
-  {item.id}
-))} */}
+      <Fragment>
+        <h1>
+          {" "}
+          <a href="#">
+            <img className="logo" src={logo} alt="logo" />
+          </a>
+          Certificates:
+        </h1>
+        <section>
+          <div className="row">
+           
+              {/*cer 1*/}
 
-  </div>)
+              <div className="product">
+                <div className="product-thumb">
+                  <a href="#">
+                    <img
+                      className="image"
+                      src={certificate1}
+                      alt="certificate"
+                    />
+                  </a>
+                </div>
+              </div>
 
-    };
+              {/*cer 2*/}
+
+              <div className="product">
+                <div className="product-thumb">
+                  <a href="#">
+                    <img
+                      className="image"
+                      src={certificate2}
+                      alt="certificate"
+                    />
+                  </a>
+                </div>
+              </div>
+
+              <div className="product">
+                <div className="product-thumb">
+                  <a href="#">
+                    <img
+                      className="image"
+                      src={certificate3}
+                      alt="certificate"
+                    />
+                  </a>
+                </div>
+              </div>
+
+              <div className="product">
+                <div className="product-thumb">
+                  <a href="#">
+                    <img
+                      className="image"
+                      src={certificate4}
+                      alt="certificate"
+                    />
+                  </a>
+                </div>
+              </div>
+           
+          </div>
+        </section>
+      </Fragment>
+    </div>
+  );
+};
+
 export default ProductCard;
