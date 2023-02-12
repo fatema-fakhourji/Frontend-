f9122f0a72b2943ceeada10d51046aee00c3b5dd
fatema <fatemaalbatoulfakhourji@gmail.com>
2023-02-12 13:42:40 +0200

    login-register

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+// import { About, Certificates, Contact, Footer, Header, Navbar, Projects, Skills, register } from './components'
+import './App.css'
+import Register from "./components/adminDashboard/register"
+
+
+export const App = () => {
+  return (
+    <div className='App'>
+      {/* <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Certificates />
+      <Contact />
+      <Footer /> */}
+      <Register />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import './header.css';
+
+
+const Header = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+     const fetchData = async () => {
+        try {
+           const response = await fetch('http://localhost:5000/about');
+           const data = await response.json();
+           setPosts(data);
+        } catch (err) {
+           console.log(err.message);
+        }
+     };
+
+     fetchData();
+  }, []);
+
+  
+  return (
+    <div>
+      {posts.map(item => (
+     
+  <div className='header-text' key={item.id}>
+    <h1>{item.quote}</h1>
+    {/* <h1>“Helping your <span className='text-special'> products </span><br /> to see the light is my <br /> calling.”</h1> */}
+  </div>))}
+  </div>
+  )
+  
+}
+
+
+
+export default Header
